42ba63462e97fe8b62eec3cda0a8b2dcccb5b8e7
Achmad Maulana <achmad.maulana@big-solution.net>
2023-06-21 17:53:56 +0700

    update

---
+++ Learn-fundamental/2-reactjs/helloword/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h4>Helloword</h4>
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Touch Me
+        </button>
+      </header>
+ </div>
+  );
+}
+
+export default App;
