c49c4ddd8ea3a3eaccd05c3b9cd37eed95ac0ab0
Achmad Maulana <achmad.maulana@big-solution.net>
2023-06-22 17:01:36 +0700

    update

--- Learn-fundamental/2-reactjs/helloword/src/App.tsx
+++ Learn-fundamental/2-reactjs/helloword/src/App.tsx
@@ -20,16 +20,16 @@ function App() {
           <p>
             Hello there, click the button below to get the list of posts from
             the API.
-          </p>
-                
-          <div className="max-w-xs place-items-center bg-indigo-900 text-center py-4 lg:px-4">
-            <div className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
-              <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">New</span>
-              <span style={{cursor:"pointer"}} className="font-semibold mr-2 text-left flex-auto" onClick={getPosts}>Get Data from API</span>
-              <svg className="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
+              </p>
+            <div className='flex flex-wrap items-center justify-center'>
+            <div className="max-w-xs place-items-center bg-indigo-900 text-center py-4 lg:px-4">
+              <div className="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+                <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">New</span>
+                <span style={{cursor:"pointer"}} className="font-semibold mr-2 text-left flex-auto" onClick={getPosts}>Get Data from API</span>
+                <svg className="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
+              </div>
             </div>
           </div>
-
         </>
       )}
       {fetchState === FetchState.LOADING && <p>Fetching posts...</p>}
