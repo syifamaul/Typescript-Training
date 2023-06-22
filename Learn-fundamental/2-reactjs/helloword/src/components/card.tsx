3a73d4cdab488b5e80e4ed1383e4192abc0975f2
Achmad Maulana <achmad.maulana@big-solution.net>
2023-06-22 10:40:58 +0700

    update

---
+++ Learn-fundamental/2-reactjs/helloword/src/components/card.tsx
@@ -0,0 +1,24 @@
+type data = {
+    title: String
+    description: string,
+    image: string
+}
+
+export function Card(props : data) {
+    return(
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-4">
+            <img className="w-full" src={props.image} alt="Sunset in the mountains"/>
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2 text-slate-700">{props.title}</div>
+                <p className="text-gray-700 text-base">
+                    {props.description} 
+                </p>
+            </div>
+        <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+            </div>
+        </div>
+    )
+}
